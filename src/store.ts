035bc6838eaa050b5e417c2b8df6dea1af03f908
Daniel Szekely <danszekely@gmail.com>
2018-02-19 12:45:27 +0000

    Install thunk middleware

--- src/store.ts
+++ src/store.ts
@@ -1,5 +1,6 @@
 import { transactions } from './reducers/transactions';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 
 interface DebugWindow extends Window {
     __REDUX_DEVTOOLS_EXTENSION__: Function;
@@ -11,5 +12,6 @@ export const store = createStore(
             transactions
         }
     ),
+    applyMiddleware(thunk),
     (window as DebugWindow).__REDUX_DEVTOOLS_EXTENSION__ && (window as DebugWindow).__REDUX_DEVTOOLS_EXTENSION__()
 );
